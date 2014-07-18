bcc08bfff8b37abe70aa190d309e6cf1d06cfcdd
Robin Dunn <rdunn@turnitin.com>
2014-07-18 23:31:41 +0100

    Added exception catching to PostServlet.java

--- smart-writer-war/src/main/java/com/rd/smartwriter/MyServlet.java
+++ smart-writer-war/src/main/java/com/rd/smartwriter/MyServlet.java
@@ -13,17 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 public class MyServlet extends HttpServlet  {
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      
-      req.setAttribute("test", "RESULT: " + LTMain.doCheck("thiss is an sentance wit some errors."));
-      req.getRequestDispatcher("smart-writer.jsp").forward(req, resp);
-      Properties p = System.getProperties();
-      p.list(resp.getWriter());
-  }
-  @Override
-  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      //TODO: handle a post request, get text as a post parameter 
-      String text = req.getParameter("text");
-      req.setAttribute("test", "RESULT: " + LTMain.doCheck(text));
       req.getRequestDispatcher("smart-writer.jsp").forward(req, resp);
       Properties p = System.getProperties();
       p.list(resp.getWriter());
--- smart-writer-war/src/main/java/com/rd/smartwriter/PostServlet.java
+++ smart-writer-war/src/main/java/com/rd/smartwriter/PostServlet.java
@@ -11,11 +11,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class PostServlet extends HttpServlet  {
-  @Override
-  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      String lang = req.getParameter("language");
-      String text = req.getParameter("text");
-      resp.setContentType("text/html");
-      resp.getWriter().print( LTMain.doCheck(lang, text) );
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String lang = req.getParameter("language");
+        String text = req.getParameter("text");
+        resp.setContentType("text/html");
+
+        String results = "";
+
+        try{
+            LTMain.doCheck(lang, text);   
+        }catch(Exception ex){
+            throw new ServletException("Exception thrown in PostServlet doPost()",ex);
+        }
+      
+        resp.getWriter().print( results );
   }
 }
